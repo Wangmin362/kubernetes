63d51c547cf3dd34fe6a7a3ca545aa1c094d8f70
wangmin <wangmin@skyguard.com.cn>
2022-04-28 13:54:32 +0800

    docs: 添加informer测试文件

---
+++ staging/src/k8s.io/client-go/informers/example/informer_basic_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"time"
+
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"log"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func main() {
+	kubeconfig := "staging/src/k8s.io/client-go/informers/example/config"
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	//表示每分钟进行一次resync，resync会周期性地执行List操作
+	sharedInformers := informers.NewSharedInformerFactory(clientset, time.Minute)
+
+	informer := sharedInformers.Core().V1().Pods().Informer()
+
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			mObj := obj.(v1.Object)
+			log.Printf("New Pod Added to Store: %s", mObj.GetName())
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			oObj := oldObj.(v1.Object)
+			nObj := newObj.(v1.Object)
+			log.Printf("%s Pod Updated to %s", oObj.GetName(), nObj.GetName())
+		},
+		DeleteFunc: func(obj interface{}) {
+			mObj := obj.(v1.Object)
+			log.Printf("Pod Deleted from Store: %s", mObj.GetName())
+		},
+	})
+
+	informer.Run(stopCh)
+}
